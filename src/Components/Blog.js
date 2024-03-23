c075ba20f56274c1b25550b74c201627b7988c2e
Prashant Agrawal <prashantagrawal2107@gmail.com>
2024-03-23 17:13:48 +0530

    Objects in useSate() and Delete Blog

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -3,8 +3,9 @@ import { useState } from "react";
 //Blogging App using Hooks
 export default function Blog(){
 
-    const [title , setTitle] = useState("");
-    const [content , setContent] = useState("");
+    // const [title , setTitle] = useState("");
+    // const [content , setContent] = useState("");
+    const [formData , setFormData] = useState({title : "" , content : ""});
     const [blogs , setBlogs] = useState([]);
     
     //Passing the synthetic event as argument to stop refreshing the page on submit
@@ -12,9 +13,16 @@ export default function Blog(){
         e.preventDefault();
         console.log(blogs);
         // setBlogs({title , content});   // Previous items will be lost
-        setBlogs([{title , content},...blogs]);  // Using REST operator to get previous items as it is.
-        setTitle("");
-        setContent("");
+        setBlogs([{title : formData.title , content : formData.content},...blogs]);  // Using REST operator to get previous items as it is.
+        // setTitle("");
+        // setContent("");
+        setFormData({title:"",content:""})
+    }
+
+    function removeBlog(index){
+        // setBlogs(blogs.filter((blog,i)=>index!==i));
+        blogs.splice(index , 1);
+        setBlogs([...blogs]);
     }
 
     return(
@@ -32,8 +40,8 @@ export default function Blog(){
                 <Row label="Title">
                         <input className="input"
                                 placeholder="Enter the Title of the Blog here.." 
-                                value={title} 
-                                onChange={(e)=> setTitle(e.target.value)}
+                                value={formData.title} 
+                                onChange={(e)=> setFormData({title : e.target.value , content : formData.content})}
                         />
                 </Row >
 
@@ -41,8 +49,8 @@ export default function Blog(){
                 <Row label="Content">
                         <textarea className="input content"
                                 placeholder="Content of the Blog goes here.."
-                                value={content}
-                                onChange={(e)=> setContent(e.target.value)}
+                                value={formData.content}
+                                onChange={(e)=> setFormData({title : formData.title , content : e.target.value})}
                         />
                 </Row >
 
@@ -60,6 +68,11 @@ export default function Blog(){
             <div className="blog" key={i}>
                 <h3>{blog.title}</h3>
                 <p>{blog.content}</p>
+                <div className="blog-btn">
+                    <button onClick={()=>removeBlog(i)} className="btn remove">
+                        Delete
+                    </button>
+                </div>
             </div>
         ))}
         </>
